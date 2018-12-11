ff506aedf523228403e94cc0ae29b4797a9ed2d0
Joglekar <ajoglekar@elogex.com>
2018-12-11 13:56:50 +0530

    Initial Actual Commit

---
+++ js/Tadekam.js
@@ -0,0 +1,13 @@
+function menuSelected(event,menuID) {
+    console.log("menuSelected::",menuID);
+    console.log("event::",event);
+    event.stopPropagation();
+    var menuSelectorClass="menuOptionTextSelected";
+    $('.' + menuSelectorClass).removeClass(menuSelectorClass);
+    $('#' + menuID).addClass(menuSelectorClass);
+    $('#' + menuID + "Hamburger").addClass(menuSelectorClass);
+    var mainContentSelectorClass="mainContentSelected";
+    $('.' + mainContentSelectorClass).removeClass(mainContentSelectorClass);
+    $('#mainContent' + menuID).addClass(mainContentSelectorClass);
+    $('#buttonHamburgerClose').click();
+}
