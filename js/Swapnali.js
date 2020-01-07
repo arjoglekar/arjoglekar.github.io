6ad35cc1eb48109e09b959f2c72a6f2cb8969893
Abhijit Joglekar <ajoglekar@elogex.com>
2020-01-07 12:18:43 +0530

    Portfolio Detail

--- js/Swapnali.js
+++ js/Swapnali.js
@@ -1,13 +1,19 @@
 function menuSelected(event,menuID) {
-    console.log("menuSelected::",menuID);
-    console.log("event::",event);
     event.stopPropagation();
     var menuSelectorClass="menuOptionTextSelected";
     $('.' + menuSelectorClass).removeClass(menuSelectorClass);
     $('.menuOptionText#' + menuID).addClass(menuSelectorClass);
-    //$('#' + menuID + "Hamburger").addClass(menuSelectorClass);
     var mainContentSelectorClass="mainContentSelected";
     $('.' + mainContentSelectorClass).removeClass(mainContentSelectorClass);
     $('#mainContent' + menuID).addClass(mainContentSelectorClass);
     $('#buttonHamburgerClose').click();
+}
+
+function portfolioSelected(event,portfolioDetailName) {
+    var menuID = portfolioDetailName.split("_")[0];
+    var portfolioName = portfolioDetailName.split("_")[1];
+    menuSelected(event,menuID);
+    var portfolioSelectorClass="portfolioDetailImageHolderSelected";
+    $('.' + portfolioSelectorClass).removeClass(portfolioSelectorClass);
+    $('#portfolioDetail' + portfolioName).addClass(portfolioSelectorClass);
 }
